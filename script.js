95a8cee7a2cbddcb072c046d86c100fa8bb941eb
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-03-28 19:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Ask for confirmation
+var confirmation = confirm("Do you want to proceed?");
+
+// Check user's response
+if (confirmation) {
+    alert("You clicked OK. Proceeding...");
+} else {
+    alert("You clicked Cancel. Exiting...");
+}
